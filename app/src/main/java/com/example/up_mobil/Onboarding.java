f15c28a326afd5b0cb454b9c3653a888098471a2
BorisovaLena <elenabor2003@gmail.com>
2023-03-06 16:26:19 +0300

    commit

--- app/src/main/java/com/example/up_mobil/Onboarding.java
+++ app/src/main/java/com/example/up_mobil/Onboarding.java
@@ -2,7 +2,9 @@ package com.example.up_mobil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Onboarding extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Onboarding extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+    }
+
+    public void onClickRegister(View v)
+    {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+
     }
 }
